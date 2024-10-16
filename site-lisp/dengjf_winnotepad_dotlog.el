d52997144f905ec68145ecd01076fdae7699b91f
DengJianfeng <dengjfzh@gmail.com>
2024-10-16 09:38:29 +0800

    自动添加日期时间之后，再自动添加一个\t

--- site-lisp/dengjf_winnotepad_dotlog.el
+++ site-lisp/dengjf_winnotepad_dotlog.el
@@ -1,4 +1,4 @@
-;; 当第一行是'.LOG\r\n'时，Windows notepad会在打开文件后跳转到最后一行，然后插入当前日期时间
+;; 当第一行是'.LOG'时，Windows notepad会在打开文件后跳转到最后一行，然后插入当前日期时间
 ;; 这个特性用来写工作日志非常好用，因此写了这个脚本来用emacs实现类似效果
 
 (defun dengjf-winnotepad-dotlog ()
@@ -9,7 +9,7 @@
 	(progn
 	  (message "dengjf-winnotepad-dotlog: head=%s, matched." head)
 	  (goto-char (point-max))
-	  (insert (format-time-string "\n%H:%M %Y/%m/%d\n"))
+	  (insert (format-time-string "\n%H:%M %Y/%m/%d\n\t"))
 	  )
         (message "dengjf-winnotepad-dotlog: head=%s, mismatched" head)
       )))
